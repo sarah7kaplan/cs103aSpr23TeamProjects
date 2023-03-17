342477909f9ae0ae536a9dc44d21d885fe3ae89a
XinyiShang <shangxinyi@126.com>
2023-03-17 10:24:46 -0400

    add bio, add method summarization
    add xinyi_shang bio to the team page, add the method summarization, add team&about page to the index page.

--- ca01/gptwebapp.py
+++ ca01/gptwebapp.py
@@ -33,21 +33,33 @@ def index():
     ''' display a link to the general query page '''
     print('processing / route')
     return f'''
+        <h1>Team</h1>
+        <a href="{url_for('team_index')}">Check our team!</a>
+        <br>
+        <h1>About</h1>
+        <a href="{url_for('about')}">Check what we are doing for this project!</a>
+        <br>
         <h1>GPT Demo</h1>
         <a href="{url_for('gptdemo')}">Ask questions to GPT</a>
+        <br>
+        <a href="{url_for('summarization')}">Summarize an article with GPT</a>
     '''
 
 @app.route('/about')
 def about():
     return "Our program..."
 
+    
 @app.route('/index')
 def team_index():
     '''display an index of team members'''
     return f'''
     <h1>Team Members</h1>
         <a href="{url_for('sarah_kaplan')}">View Sarah Kaplan's bio!</a>
+        <br>
         <a href="{url_for('michael_pyrdol')}">View Michael Pyrdol's bio!</a>
+        <br>
+        <a href="{url_for('xinyi_shang')}">View Xinyi Shang's bio!</a>
     '''
 
 @app.route('/sarah_kaplan')
@@ -66,6 +78,13 @@ def michael_pyrdol():
         and minoring in Anthropology. He plays the clarinet in various ensembles \
         on campus, and he is also a music director for Top Score, \
         the student-led orchestra."
+
+@app.route('/xinyi_shang')
+def xinyi_shang():
+     return '''
+    <h1>Xinyi Shang</h1>
+    Definitely a dog person. 
+    '''
 
 # @app.route('/TEAM_MEMBER')
 # def TEAM_MEMBER():
@@ -96,6 +115,33 @@ def gptdemo():
         <form method="post">
             <textarea name="prompt"></textarea>
             <p><input type=submit value="get response">
+        </form>
+        '''
+
+@app.route('/summarization', methods=['GET', 'POST'])
+def summarization():
+    #Method: get summarization of an article. 
+    ''' handle a get request by sending a form 
+        and a post request by returning the GPT summary
+    '''
+    if request.method == 'POST':
+        article = request.form['article']
+        summary = gptAPI.get_summarization(article)
+        return f'''
+        <h1>Summarization Demo</h1>
+        <pre style="bgcolor:yellow">{article}</pre>
+        <hr>
+        Here is the summarys:
+        <div style="border:thin solid black">{summary}</div>
+        <a href={url_for('summarization')}> summarize another article</a>
+        '''
+    else:
+        return '''
+        <h1>Summarization Demo App</h1>
+        Enter your article below
+        <form method="post">
+            <textarea name="article"></textarea>
+            <p><input type=submit value="get summary">
         </form>
         '''
 
