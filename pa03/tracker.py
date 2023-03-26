2baa06a7e824d73fc73f1aa4ea4d892a23ff81c3
MichaelPyrdol <michaelpyrdol@brandeis.edu>
2023-03-26 17:53:55 -0400

    Update tracker.py

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -15,7 +15,7 @@
 import sqlite3
 from transaction import Transaction
 filename=""
-userInput=input("0. quit\n1. show categories\n2. add category\n3. modify category\n4. show transactions\n5. add transaction\n6. delete transaction\n7. summarize transactions by date\n8. summarize transactions by month\n9. summarize transactions by year\n10. summarize transactions by category\n11. print this menu\n")
+userInput=input("Please select an action:\n0. quit\n1. show categories\n2. add category\n3. modify category\n4. show transactions\n5. add transaction\n6. delete transaction\n7. summarize transactions by date\n8. summarize transactions by month\n9. summarize transactions by year\n10. summarize transactions by category\n11. print this menu\n")
 if userInput==0:
     quit
 if userInput==1:
