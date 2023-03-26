aa1186760e24b413f97c7bc187c67c4b2264bebd
XinyiShang <shangxinyi@126.com>
2023-03-26 17:11:55 -0400

    add method add_transaction

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -4,4 +4,11 @@ import os
 class Transaction:
     filename=""
     def __init__(self,filename):
-        self.filename
+        self.filename = filename
+        self.create_table()
+    def add_transaction(self, item_number, amount, category, date, description):
+        with sqlite3.connect(self.db_file) as conn:
+            c = conn.cursor()
+            c.execute('''INSERT INTO transactions (item_number, amount, category, date, description)
+                            VALUES (?, ?, ?, ?, ?)''', (item_number, amount, category, date, description))
+            conn.commit()
