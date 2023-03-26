6cb34e86eff5af1a7935ee2e9f1d1bc5c908a0d1
sarah7kaplan <123101045+sarah7kaplan@users.noreply.github.com>
2023-03-26 17:10:15 -0400

    added imports and options description

---
+++ pa03/tracker.py
@@ -0,0 +1,13 @@
+# Options:
+# 0. quit
+# 1. show categories
+# 2. add category
+# 3. modify category
+# 4. show transactions
+# 5. add transaction
+# 6. delete transaction
+# 7. summarize transactions by date
+# 8. summarize transactions by month
+# 9. summarize transactions by year
+# 10. summarize transactions by category
+# 11. print this menu
--- pa03/transaction.py
+++ pa03/transaction.py
@@ -1,3 +1,6 @@
+import sqlite3
+import os
+
 class Transaction:
     filename=""
     def __init__(self,filename):
