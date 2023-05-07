0946f528fa18a073b121e7856976c8c19295dde6
MichaelPyrdol <michaelpyrdol@brandeis.edu>
2023-05-07 14:46:30 -0400

    Cleaning up code

--- ca02/routes/michael.js
+++ ca02/routes/michael.js
@@ -1,10 +1,9 @@
 /*
-  This utilizes ChatGPT
+  This is the part that gets the GPT API.
 */
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-
 isLoggedIn = (req,res,next) => {
   if (res.locals.loggedIn) {
     next()
@@ -12,11 +11,9 @@ isLoggedIn = (req,res,next) => {
     res.redirect('/login')
   }
 }
-
 router.get('/michael', isLoggedIn, (req,res) => {
     res.render('michael')
 })
-
 router.post('/api/v1/prompt/michael', isLoggedIn, async (req,res) => {
   if (!req.body.prompt) {res.status(400).json({error: 'prompt is required'}).send(); return}
   const response = await axios.post('http://gracehopper.cs-i.brandeis.edu:3500/openai',
@@ -24,5 +21,4 @@ router.post('/api/v1/prompt/michael', isLoggedIn, async (req,res) => {
   res.status(200).send(response.data.choices[0]['message']['content']);
   return;
   })
-
   module.exports = router;
